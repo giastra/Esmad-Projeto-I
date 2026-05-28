2a44e466771747ce88c666a1d696ae403a10d503
giastra <giastra>
2026-05-28 13:55:18 +0100

    Extensão, adapitação do codigo das tarefas para suportar uma lista de objetos, botões todas e prioridade funcionais e preparações para a alicação do sistema MVC

--- extentsao/js/script.js
+++ extentsao/js/script.js
@@ -1,64 +1,140 @@
-
 let pin = "img/pin.png"
 let pinIN = "img/pinIN.png"
 
 //tem que rever essa parte com a integração da api
-let SImg = "img/SAmarelo.png"
+function SImg(objeto) { 
+    if (objeto == "alta")
+        {return "img/SVermelho.png"}
+    else if (objeto == "normal")
+        {return "img/SAmarelo.png"}
+    else if (objeto == "baixa")
+        {return "img/SVerde.png"}
+    else {return ""}
+}
 
-let tasks = ["limpar o chão","Fazer o dever de casa","Trabalhar muito"]
-
-function todas(){
-for (let num in tasks) {
-    document.getElementById("tarefas").innerHTML+=`
-            <div class="tasks" name="${tasks[num]}">
+function Rend(objeto,Tpin=pin){`
+            <div class="tasks" name="${objeto.name}">
             <div class='linhaTxt'>
             <input type="checkbox" >
-            <p>${tasks[num]}</p>
+            <p>${objeto.name}</p>
             </div>
             <div class='pin'>
-            <img src="${SImg}" width="15px" height="15px">
-             <img src="${pin}" name="${tasks[num]}" width="15px" height="15px">
+             <img src="${SImg(objeto.priority)}" width="15px" height="15px">
+             <img src="${pin}" name="${objeto.name}" width="15px" height="15px">
             </div>
-            </div>`  
-}
-    document.getElementById("telaInicial").innerHTML=`
+            </div>` }
+
+function RendBtn(){
+    `
         <p id="tarefaAtiva">Tarefa Prioritaria</p>
         <hr>
         <div class="botoes">
-            <button name="Todas" class="botao">Todas</button>
-            <button name="Prioridade" class="botao">Prioridade</button>
+            <button id="Todas" class="botao">Todas</button>
+            <button id="Prioridade" class="botao">Prioridade</button>
         </div>
         <div class="botoes">
-            <button name="Hoje" class="botao">Hoje</button>
-            <button name="EstaSemana" class="botao">Esta Semana</button>
+            <button id="Hoje" class="botao">Hoje</button>
+            <button id="EstaSemana" class="botao">Esta Semana</button>
         </div>`
 }
 
-todas()
+const tasks = [
+    {
+        name : "Limpar o chão",
+        description : "limpe",
+        startDate : "2002-12-04",
+        endDate : "2022-02-12",
+        status : "por_fazer",
+        priority: "normal",
+        user : {name : "giastra"},
+        category : {
+            name : "tarefas",
+            color : "#5C95FF"
+        }
+    },
+     {
+        name : "Fazer o dever de casa",
+        description : "dever",
+        startDate : "2022-12-04",
+        endDate : "2022-02-12",
+        status : "por_fazer",
+        priority: "alta",
+        user : {name : "giastra"},
+        category : {
+            name : "tarefas",
+            color : "#5C955F"
+        }
+    },
+    {
+        name :"Trabalhar muito",
+        description : "dever",
+        startDate : "2022-12-04",
+        endDate : "2002-02-12",
+        status : "por_fazer",
+        priority: "baixa",
+        user : {name : "giastra"},
+        category : {
+            name : "tarefas",
+            color : "#FC95FF"}
+    }]
+
+function todas(){
+    document.getElementById("tarefas").innerHTML=''
+for (let num in tasks) {
+    document.getElementById("tarefas").innerHTML+=Rend(tasks[num])
+}
+    document.getElementById("telaInicial").innerHTML=RendBtn
+    
+        // todas btn
+        document.getElementById("Todas").addEventListener('click',function(){
+            console.log('todas');
+            todas()
+        })
+
+        // prioridade btn
+        document.getElementById("Prioridade").addEventListener('click',function(){
+        console.log('Prioridade');
+        document.getElementById("tarefas").innerHTML=``
+        const listaA=[]
+        const listaN=[]
+        const listaB=[]
+        for (let trab in tasks){    
+            if (tasks[trab].priority=='alta'){
+                listaA.push(tasks[trab])
+            }
+            else if (tasks[trab].priority=='normal'){
+                listaN.push(tasks[trab])
+            }
+            else if (tasks[trab].priority=='baixa'){
+                listaB.push(tasks[trab])
+            }}
+
+        const lista = []
+        listaA.map(n => lista.push(n))
+        listaN.map(n => lista.push(n))
+        listaB.map(n => lista.push(n))
+        for (let num in lista){
+        document.getElementById("tarefas").innerHTML+=Rend(lista[num])
+              
+        }})
+}
+
+// inicial 
+
 let statu=false
  let num =0
 document.getElementById('tarefas').addEventListener('click',function(){
     let pin=(event.target.getAttribute('name'));
    
     for (let trab in tasks){    
-        if (tasks[trab]==pin){
+        if (tasks[trab].name==pin){
             num = trab
             console.log(trab);
         }}
 
 
         if (statu == false && pin!=null){
-            document.getElementById("tarefas").innerHTML=`
-            <div class="tasks" name="${tasks[num]}">
-            <div class='linhaTxt'>
-            <input type="checkbox" >
-            <p>${tasks[num]}</p>
-            </div>
-            <div class='pin'>
-             <img src="${SImg}" width="15px" height="15px">
-             <img src="${pinIN}" name="${tasks[num]}" width="15px" height="15px">
-            </div>
-            </div>`   
+            document.getElementById("tarefas").innerHTML=Rend(lista[num],pinIN)   
             statu=true
 
             document.getElementById("telaInicial").innerHTML=``
