3f97e085e658384c3e82d65af68070e5257ba1ca
giastra <giastra>
2026-05-28 17:59:26 +0100

    Estensão checkbox atualiza o objeto e cancelamento do metodo mvc para a extensão

--- extentsao/js/script.js
+++ extentsao/js/script.js
@@ -2,49 +2,13 @@ let pin = "img/pin.png"
 let pinIN = "img/pinIN.png"
 
 //tem que rever essa parte com a integração da api
-function SImg(objeto) { 
-    if (objeto == "alta")
-        {return "img/SVermelho.png"}
-    else if (objeto == "normal")
-        {return "img/SAmarelo.png"}
-    else if (objeto == "baixa")
-        {return "img/SVerde.png"}
-    else {return ""}
-}
-
-function Rend(objeto,Tpin=pin){return`
-            <div class="tasks" name="${objeto.name}">
-            <div class='linhaTxt'>
-            <input type="checkbox" >
-            <p>${objeto.name}</p>
-            </div>
-            <div class='pin'>
-             <img src="${SImg(objeto.priority)}" width="15px" height="15px">
-             <img src="${Tpin}" name="${objeto.name}" width="15px" height="15px">
-            </div>
-            </div>` }
-
-function RendBtn(){
-    return`
-        <p id="tarefaAtiva">Tarefa Prioritaria</p>
-        <hr>
-        <div class="botoes">
-            <button id="Todas" class="botao">Todas</button>
-            <button id="Prioridade" class="botao">Prioridade</button>
-        </div>
-        <div class="botoes">
-            <button id="Hoje" class="botao">Hoje</button>
-            <button id="EstaSemana" class="botao">Esta Semana</button>
-        </div>`
-}
-
 const tasks = [
     {
         name : "Limpar o chão",
         description : "limpe",
         startDate : "2002-12-04",
         endDate : "2022-02-12",
-        status : "por_fazer",
+        status : "concluida",
         priority: "normal",
         user : {name : "giastra"},
         category : {
@@ -76,19 +40,98 @@ const tasks = [
         category : {
             name : "tarefas",
             color : "#FC95FF"}
-    }]
+    }
+]
 
-function todas(){
-    document.getElementById("tarefas").innerHTML=''
-for (let num in tasks) {
-    document.getElementById("tarefas").innerHTML+=Rend(tasks[num])
+// atribuição da bola de prioridade para cada nivel
+function SImg(objeto) { 
+    if (objeto == "alta")
+        {return "img/SVermelho.png"}
+
+    else if (objeto == "normal")
+        {return "img/SAmarelo.png"}
+
+    else if (objeto == "baixa")
+        {return "img/SVerde.png"}
+
+    else {return "img/LogoNeutro"}
 }
-    document.getElementById("telaInicial").innerHTML=RendBtn()
+
+function check(objeto) {
+    if (objeto.status == 'concluida'){
+        return  (true)
+    }
+}
+
+// renderiza a caixa de tarefa a partir da lista dada e podendo estar com pin ou sem pin(objeto,pin ou pinIN)
+function Rend(objeto,Tpin=pin){
+    if (check(objeto)){
+        return`
+            <div class="tasks">
+            <div class='linhaTxt'>
+            <input type="checkbox" id='checkbox' value='${objeto.name}' checked  >
+            <p>${objeto.name}</p>
+            </div>
+            <div >
+             <img src="${SImg(objeto.priority)}" width="20px" height="20px">
+             <img class='pin' src="${Tpin}" name="${objeto.name}" width="20px" height="20px">
+            </div>
+            </div>
+            ` 
+    }
     
+    else{
+    return`
+            <div class="tasks">
+            <div class='linhaTxt'>
+            <input type="checkbox" id='checkbox' value='${objeto.name}'  >
+            <p>${objeto.name}</p>
+            </div>
+            <div >
+             <img src="${SImg(objeto.priority)}" width="20px" height="20px">
+             <img class='pin' src="${Tpin}" name="${objeto.name}" width="20px" height="20px">
+            </div>
+            </div>
+            ` }
+}
+
+// os quatro botões iniciais  
+function RendBtn(){
+    return`
+        <p id="tarefaAtiva">Tarefa Prioritaria</p>
+        <hr>
+        <div class="botoes">
+            <button id="Todas" class="botao">Todas</button>
+            <button id="Prioridade" class="botao">Prioridade</button>
+        </div>
+        <div class="botoes">
+            <button id="Hoje" class="botao">Hoje</button>
+            <button id="EstaSemana" class="botao">Esta Semana</button>
+        </div>`
+}
+
+// renderização gral das tarefas e a reatividade dos botões 
+function todas(tipo=0){
+    document.getElementById("tarefas").innerHTML=''
+    if (tipo == 1){
+        for (let num in ListUltima) {
+            document.getElementById("tarefas").innerHTML+=Rend(ListUltima[num])
+        }
+        document.getElementById("telaInicial").innerHTML=RendBtn()
+    }
+
+    else{
+        for (let num in tasks) {
+            document.getElementById("tarefas").innerHTML+=Rend(tasks[num])
+        }
+        document.getElementById("telaInicial").innerHTML=RendBtn()
+    }
+
         // todas btn
-    document.getElementById("Todas").addEventListener('click',function(){
+        document.getElementById("Todas").addEventListener('click',function(){
             console.log('todas');
             todas()
+             ListUltima = tasks
         })
 
         // prioridade btn
@@ -116,34 +159,62 @@ for (let num in tasks) {
         for (let num in lista){
         document.getElementById("tarefas").innerHTML+=Rend(lista[num])
         }
+        ListUltima = lista
     })
 }
 
-// inicial 
-todas()
-let statu=false
- let num =0
+
+
+
+// reatividade ao clicar em cada tarefa para ser fixada
 document.getElementById('tarefas').addEventListener('click',function(){
-    let pin=(event.target.getAttribute('name'));
-   
-    for (let trab in tasks){    
-        if (tasks[trab].name==pin){
+    let ev=(event.target.getAttribute('name'));
+    let va = (event.target.getAttribute('value')) 
+    
+    for (let trab in tasks){ 
+            
+        // seleção de foco
+        if (tasks[trab].name==ev){
             num = trab
             console.log(trab);
-        }}
+        }
 
-
-        if (statu == false && pin!=null){
+        if (statu == false && ev!=null){
             document.getElementById("tarefas").innerHTML=Rend(tasks[num],pinIN)   
             statu=true
 
             document.getElementById("telaInicial").innerHTML=``
         }
 
-        else{
+        else if (statu == true && ev!=null){
             statu=false
             document.getElementById("tarefas").innerHTML=''
-            todas()
+            todas(1)
         }
-})
+        
+        
+        // seleção de checkbox
+        if (tasks[trab].name==va){
+            console.log(trab);
+            if (tasks[trab].status=='por_fazer'){
+                tasks[trab].status='concluida'
+                console.log(tasks[trab].status);
+                
+            }
+            else{tasks[trab].status='por_fazer'
+                console.log(tasks[trab].status);
+            }
+        }
+    }
+}
 
+
+      
+         
+)
+
+// inicial 
+todas()
+let ListUltima = tasks
+let statu=false
+let num =0
