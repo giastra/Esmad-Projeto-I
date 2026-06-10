ab712623cc115cf561a53127420489d25279f102
giastra <giastra>
2026-06-10 20:09:05 +0100

    pg Home, correção do display das categorias e das tarefas e do baner da extensão

--- js/view/categoriasView.js
+++ js/view/categoriasView.js
@@ -75,7 +75,7 @@ export function criarCard(id,titulo,color,ncomp=0,ntotal=0,temCategoria='s') {
     card.classList.add("category-card",id);
 
     card.innerHTML = `
-    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;" id='categoria' name="${id}">
+    <div class="card p-4 mb-3" id='categoria' name="${id}">
         <h5 class="card-title mb-2" style="font-size: 16px;">${titulo}</h5>
 
         <div class="d-flex align-items-center gap-2">
@@ -103,7 +103,7 @@ else{
     card.classList.add("category-card");
 
     card.innerHTML = `
-    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;" id='categoria'>
+    <div class="card p-4 mb-3" style="width: 290px; border-radius: 12px" id='categoria'>
         <h5 class="card-title mb-2" style="font-size: 16px;">Nenhuma categoria criada</h5>
     </div>
     `;
--- js/view/homeView.js
+++ js/view/homeView.js
@@ -78,7 +78,7 @@ export function criarCard(titulo,color,ncomp=0,ntotal=0,temCategoria='s') {
     card.classList.add("category-card");
 
     card.innerHTML = `
-    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;" id='categoria'>
+    <div class="card p-4 mb-3" id='categoria'>
         <h5 class="card-title mb-2" style="font-size: 16px;">${titulo}</h5>
 
         <div class="d-flex align-items-center gap-2">
