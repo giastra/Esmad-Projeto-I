f8c2c4256749c9d8ef3b68d039bdce8f13d43fb2
giastra <giastra>
2026-06-10 18:17:54 +0100

    pg Categoria feita, com as posibilidades de escolher cores, salvar as tarefas na api, mostrar na tela, e a possibilidade de clicar nelas e levar para a parte de tarefas especifica. Falta somente os botões que ligam as tarefas com filtro

--- js/controller/taskCategoryController.js
+++ js/controller/taskCategoryController.js
@@ -1,12 +1,28 @@
 import { getMinhasCategorias, criar, apagar } from '../model/taskCategoryModel.js';
-import { getColors } from '../model/colorModel.js';
+import { getColors,criarCor } from '../model/colorModel.js';
 import { requireAuth } from '../utils/helpers.js';
+import {criarCard} from "../view/categoriasView.js"
+import { getMinhasTarefas } from '../model/taskModel.js';
+import {getMe} from '../model/userModel.js'
+
+
+const cores= await getColors()
+if ((cores.data).length==0){
+  const dat ={
+    name:"CornflowerBlue",
+    hex:"#5C95FF"
+  }
+  console.log('corCriada');
+  
+  criarCor(dat)
+}
 
 const resCat = await getMinhasCategorias();
-const cat = res.data
-console.log(cat);
+const cat = resCat.data
 const resColor = await getColors();
 const color = resColor.data
+const rs = await getMinhasTarefas()
+const tasks = rs.data
 
 // Carrega todas as categorias do utilizador
 export function carregarCategorias() { 
@@ -21,56 +37,46 @@ export function carregarCategorias() {
                       nconclu ++
                   }
               }
-          }
-          criarCard(tas.name,tas.color,nconclu,ntotal)
+          }    
+          criarCard(tas._id,tas.name,tas.color,nconclu,ntotal)
       }
   }
   else{
-      criarCard('n','n','n','n','n')
+      criarCard('n','n','n','n','n','n')
   }
-  
 };
 
 // Carrega as cores para o select do formulário
 export function carregarCores(){
- 
-  
+ return color
+}
+
+export function apagarCategoria(id){
+  apagar(id)
 }
 
 // Liga os eventos aos botões da lista
-const bindEventos = () => {
-  document.querySelectorAll('.btn-apagar').forEach(btn => {
-    btn.addEventListener('click', async (e) => {
-      const id = e.target.closest('[data-id]').dataset.id;
-      const confirmar = confirm('Tens a certeza que queres apagar esta categoria?');
-      if (!confirmar) return;
+// const bindEventos = () => {
+//   document.querySelectorAll('.btn-apagar').forEach(btn => {
+//     btn.addEventListener('click', async (e) => {
+//       const id = e.target.closest('[data-id]').dataset.id;
+//       const confirmar = confirm('Tens a certeza que queres apagar esta categoria?');
+//       if (!confirmar) return;
 
-      const res = await apagar(id);
-      if (res.success) carregarCategorias();
-      else renderErro(res.message);
-    });
-  });
-};
+//       const res = await apagar(id);
+//       if (res.success) carregarCategorias();
+//       else renderErro(res.message);
+//     });
+//   });
+// };
 
 // CRIAR CATEGORIA
-document.getElementById('form-category')?.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  limparMensagens();
+export function criarCategoria(nome,cor=color[0]._id){
 
-  const res = await criar({
-    name: document.getElementById('name').value,
-    color: document.getElementById('color').value
-  });
-
-  if (res.success) {
-    renderSucesso('Categoria criada com sucesso.');
-    e.target.reset();
-    carregarCategorias();
-  } else {
-    renderErro(res.message);
+  const data = {
+    name:nome,
+    color:cor
   }
-});
-
-
-// carregarCores();
-// carregarCategorias();
+  criar(data)
+  window.location.reload();
+}
--- js/view/categoriasView.js
+++ js/view/categoriasView.js
@@ -1,4 +1,4 @@
-import {carregarCores,carregarCategorias} from "../controller/taskCategoryController.js"
+import {carregarCores,carregarCategorias,apagarCategoria,criarCategoria} from "../controller/taskCategoryController.js"
 /* BOTÕES E ELEMENTOS BASE */
 
 const btnAdd = document.querySelector(".btn-add");
@@ -6,26 +6,25 @@ const middle = document.getElementById("middle");
 const btnDeleteCategorias = document.querySelector(".btn-delete");
 
 let modoEliminarCategoria = false;
-carregarCategorias()
+
 
 /* ABRIR MODAL DE CRIAR CATEGORIA */
 
 btnAdd.addEventListener("click", () => {
     criarModal();
 });
-console.log(carregarCores());
 
+// configuração padrão de criação de card e modal
 function criarModal() {
     const modal = document.createElement("div");
     modal.classList.add("modal");
-
     modal.innerHTML = `
         <div class="modal-content">
             <h2>Criar Categoria</h2>
 
             <label>Título</label>
             <input type="text" id="tituloCategoria" placeholder="Nome da categoria">
-            <input type='button' id="" >
+            <section id='cores'></section>
             <button id="criarCategoria" class="btn-modal-add">Adicionar</button>
         </div>
     `;
@@ -33,11 +32,28 @@ function criarModal() {
     document.body.appendChild(modal);
     modal.style.display = "flex";
 
+    // carrega as opções de cores 
+    for (const cor of carregarCores()){ 
+    document.getElementById('cores').innerHTML+=`
+        <input type="button" style="background-color: ${cor.hex};" name="${cor._id}">
+    `
+    }
+    let CorSelecionada=""
+    document.getElementById('cores').addEventListener("click",function(){
+        console.log(event.target.getAttribute('name'));
+         CorSelecionada=event.target.getAttribute('name')
+    })
+
+
+
     modal.querySelector("#criarCategoria").addEventListener("click", () => {
         const titulo = modal.querySelector("#tituloCategoria").value.trim();
+        const cor = ''
         if (titulo === "") return;
-
-        criarCard(titulo);
+        // criar categoria recebe (titulo da tarefa,cor)
+        if (CorSelecionada != '') {criarCategoria(titulo,CorSelecionada)}
+        // caso não receba cor
+        else {criarCategoria(titulo)}
         modal.remove();
     });
 
@@ -46,27 +62,54 @@ function criarModal() {
     });
 }
 
-
 /* CRIAR CARD DE CATEGORIA */
+// (_id,titulo, cor , qauntas tarefas completas, quantas tarefas no total,se foi encontrado tarefa (s/n))
+export function criarCard(id,titulo,color,ncomp=0,ntotal=0,temCategoria='s') {
+    if (temCategoria == 's'){
+    let porcenta = 100
+    if (ntotal != 0){
+        porcenta = (ncomp*100)/ntotal
+    }
 
-function criarCard(titulo) {
     const card = document.createElement("div");
-    card.classList.add("category-card");
+    card.classList.add("category-card",id);
 
     card.innerHTML = `
-    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;">
+    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;" id='categoria' name="${id}">
         <h5 class="card-title mb-2" style="font-size: 16px;">${titulo}</h5>
 
         <div class="d-flex align-items-center gap-2">
             <div class="progress flex-grow-1" style="height: 8px; border-radius: 10px;">
-                <div class="progress-bar" role="progressbar" style="width: 0%; background:#7b4bff;"></div>
+                <div class="progress-bar" role="progressbar" style="width: ${porcenta}%; background:${color};"></div>
             </div>
-            <span style="font-size: 12px; font-weight: 600;">0/10</span>
+            <span style="font-size: 12px; font-weight: 600;">${ncomp}/${ntotal}</span>
         </div>
+    </div>
+    `;
+    middle.appendChild(card);
+
+    // adiciona a possibilidade de entrar nas categoria selecionada
+    card.addEventListener('click',function(){
+        if (modoEliminarCategoria == false){
+        localStorage.setItem("categoria", id)
+         window.location.href = "./Tarefas.html"
+        }
+    })
+}
+
+// caso não exista cards
+else{
+    const card = document.createElement("div");
+    card.classList.add("category-card");
+
+    card.innerHTML = `
+    <div class="card p-4 mb-3" style="width: 300px; border-radius: 12px;" id='categoria'>
+        <h5 class="card-title mb-2" style="font-size: 16px;">Nenhuma categoria criada</h5>
     </div>
     `;
 
     middle.appendChild(card);
+}
 }
 
 
@@ -86,6 +129,7 @@ btnDeleteCategorias.addEventListener("click", () => {
     document.querySelectorAll(".modal").forEach(m => m.remove());
 });
 
+// remove o card
 middle.addEventListener("click", (e) => {
     if (!modoEliminarCategoria) return;
 
@@ -93,8 +137,13 @@ middle.addEventListener("click", (e) => {
     if (!card) return;
 
     card.remove();
+    let id=card.getAttribute('class').split(' ')[1]
+    apagarCategoria(id)
+
 
     modoEliminarCategoria = false;
     btnDeleteCategorias.classList.remove("ativo");
 });
 
+// init
+carregarCategorias()
