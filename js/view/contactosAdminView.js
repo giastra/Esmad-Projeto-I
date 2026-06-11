e4a3fcdccedd8eff3431f1554d700cef3cec3f3f
filipecardosogoncalves-sketch <filipe.cardoso.goncalves@gmail.com>
2026-06-11 20:20:01 +0100

    Resolução de problemas na pagina contactos sobre nos, modo aumentar letras e botao pomodoro

--- js/view/contactosAdminView.js
+++ js/view/contactosAdminView.js
@@ -15,13 +15,28 @@ function carregarContactos() {
             <h4>${msg.nome}</h4>
             <p>${msg.email}</p>
             <span>Ass: ${msg.assunto}</span>
+            <button class="btn-delete" data-index="${index}">
+                <img src="../images/Trash.png" alt="Apagar">
+            </button>
         `;
 
+        // Clicar no item → mostrar mensagem
         div.addEventListener("click", () => {
             mostrarMensagem(index);
         });
 
         lista.appendChild(div);
+    });
+
+    // Ativar botões delete
+    document.querySelectorAll(".btn-delete").forEach(btn => {
+        btn.addEventListener("click", function (event) {
+            event.stopPropagation(); 
+
+            const index = this.getAttribute("data-index");
+
+            apagarMensagem(index);
+        });
     });
 }
 
@@ -45,3 +60,17 @@ function mostrarMensagem(index) {
 
 /* Carregar automaticamente ao abrir a página */
 window.onload = carregarContactos;
+
+
+//Apagar mensagem
+function apagarMensagem(index) {
+    let mensagens = JSON.parse(localStorage.getItem("contactos")) || [];
+
+    mensagens.splice(index, 1); // remove 1 elemento na posição index
+
+    localStorage.setItem("contactos", JSON.stringify(mensagens));
+
+    carregarContactos(); // atualizar lista
+
+    document.getElementById("email-content").innerHTML = ""; // limpar painel direito
+}
